6f7d31361ba901045bce1271982ab141a0745b74
Alexfox22 <nika-by-niksa@mail.ru>
2017-12-08 10:23:10 +0300

    Kruskal works, but it paints only nearest points

--- Aias/Source.cpp
+++ Aias/Source.cpp
@@ -14,12 +14,17 @@ using namespace std;
 
 struct node
 {
-	//vector<int> neighbour;
-	//vector<int> waight;
-	int *neighbour = new int[100];
-	int *waight = new int[100];
+	vector<int> neighbour;
+	vector<int> waight;
+	//int *neighbour = new int[100];
+	//int *waight = new int[100];
 	int top;
-	bool IsBlue;
+	int color;
+};
+struct rib {
+	int beg;
+	int end;
+	int wai;
 };
 struct minimum
 {
@@ -27,51 +32,104 @@ struct minimum
 	int pos;
 	int str;
 };
-void putgraph(node *gr,int tops,int edges)
+void outputio(int tops,node* masKr)
 {
-	for (int i = 0;i < tops;i++)
+	for (int i = 0;i < tops;i++)		//вывод
 	{
-		int r = 0;
-		cout << "Input key ";
-		cin >> gr[i].top;
-		cout << "Input count of neighbours ";
-		cin >> r;
-		while (r>tops-1)
+		int f = 0;
+		if (masKr[i].top > 0)
+			cout << masKr[i].top << endl;
+		while (f<masKr[i].neighbour.size())
 		{
-			cout << "Wrong value. Please, try again" << endl;
-			cin >> r;
+			cout << masKr[i].neighbour[f] << "  ";
+
+			f++;
 		}
 		cout << endl;
-		for (int j = 0;j < r;j++)
+		int fu = 0;
+		while (fu <masKr[i].waight.size())
 		{
-			cout << "Input key of neighbour ";
-			cin >> gr[i].neighbour[j];
-			cout << " Input value of edge ";
-			cin >> gr[i].waight[j];
-			cout << endl;
+			cout << masKr[i].waight[fu] << "  ";
+			fu++;
 		}
+		cout << endl;
+
+		cout << endl;
 	}
 }
-void getgraph(node *gr, int tops, int edges)
+void sortnode(rib* in,int edges)
+{
+	int lenD = edges;
+	
+	int keyw = 0;
+	int keyb = 0;
+	int keye = 0;
+	int i = 0;
+	for (int j = 1;j < lenD;j++) {
+		keyw = in[j].wai;
+		keyb = in[j].beg;
+		keye = in[j].end;
+		i = j - 1;
+		while (i >= 0 && in[i].wai > keyw)
+			{
+				in[i + 1].wai = in[i].wai;
+				in[i + 1].beg = in[i].beg;
+				in[i + 1].end = in[i].end;
+				i = i - 1;
+				in[i + 1].wai = keyw;
+				in[i + 1].beg = keyb;
+				in[i + 1].end = keye;
+			}
+		
+	}
+}
+
+//void putgraph(node *gr,int tops,int edges)
+//{
+//	for (int i = 0;i < tops;i++)
+//	{
+//		int r = 0;
+//		cout << "Input key ";
+//		cin >> gr[i].top;
+//		cout << "Input count of neighbours ";
+//		cin >> r;
+//		while (r>tops-1)
+//		{
+//			cout << "Wrong value. Please, try again" << endl;
+//			cin >> r;
+//		}
+//		cout << endl;
+//		int k = 0;
+//			cout << "Input key of neighbour ";
+//			cin >> k;
+//			gr[i].neighbour.push_back(k);
+//			cout << " Input value of edge ";
+//			cin >> k;
+//			gr[i].waight.push_back(k);
+//			cout << endl;
+//		
+//	}
+//}
+void getgraph(node *gr, int tops)
 {
 
 	for (int i = 0;i < tops;i++)
 		{  			
 			
 				cout << "top "<< gr[i].top<<"  ";
-				for (int j = 0;j < 10;j++)
+				for (int j = 0;j < gr[i].neighbour.size();j++)
 					cout << gr[i].neighbour[j] << ' ';
 				cout << endl;
 
 		}
 
 }
-void getways(node *gr, int tops, int edges)
+void getways(node *gr, int tops)
 {
 	for (int i = 0;i < tops;i++)
 	{	
 		cout << "top " << gr[i].top << "  ";
-		for (int j = 0;j < 10;j++)
+		for (int j = 0;j < gr[i].neighbour.size();j++)
 		{
 			if (gr[i].neighbour[j] > 0)
 			{
@@ -81,32 +139,36 @@ void getways(node *gr, int tops, int edges)
 		cout << endl;
 	}
 }
-void make_matrix_neigh(node *gr, int tops, int edges)
-{
-
-	for (int i = 0;i < tops;i++)
-	{
-		
-		int j = 0;
-		for (int k = 1;k < tops+1;k++)
-		{
-
-			if (gr[i].neighbour[j] != k)
-			{
-				cout << "0 ";
-			//	cout << "fasas"<<gr[i].neighbour[j]<<" "<< k<<"   ";
-				
-			}
-			else /*(gr[i].neighbour[j] == k)*/
-			{
-				cout << "1 ";
-				j++;
-			}
-		}
-		cout << endl;
-	}
-
-}
+//void make_matrix_neigh(node *gr, int tops, int edges)
+//{
+//
+//	for (int i = 0;i < tops;i++)
+//	{
+//		
+//		for (int j=0;j < gr[i].neighbour.size()j++)
+//		{
+//			for (int k = 1;k < tops + 1;k++)
+//			{
+//				
+//					if (gr[i].neighbour[j] == k)
+//					{
+//						cout << "1 ";
+//						j++;
+//							cout << "fasas"<<gr[i].neighbour[j]<<" "<< k<<"   ";
+//
+//					}
+//					else /*(gr[i].neighbour[j] == k)*/
+//					{
+//						cout << "0 ";
+//						
+//					}
+//				}
+//			
+//		}
+//		cout << endl;
+//	}
+//
+//}
 void outp(int **mas,int tops)
 {
 	for (int j = 0;j < tops;j++)
@@ -122,43 +184,28 @@ void outp(int **mas,int tops)
 	}
 	cout << endl;
 }
-void make_matrix_wei(node *gr, int tops, int edges,int **mas)
+void make_matrix_wei(node *gr, int tops,int **mas)
 {
 
 	for (int i = 0;i < tops;i++)
 	{
-		int k = 1;
-		int j = 0;
-		for (int k = 1;k < tops + 1;k++)
+		for (int j = 0;j < tops;j++)
 		{
-
-			if (gr[i].neighbour[j] != k)
-			{
-				if (k - 1 == i)
-				{
-					cout << "0 ";
-					mas[i][k - 1] = INT_MAX-1;
-					//cout << gr[i].neighbour[j]<<" "<< k<<"   ";
-				}
-				if (k-1 != i)
-				{
-					cout << "^ ";
-					mas[i][k - 1] =INT_MAX-1;
-					//cout << gr[i].neighbour[j]<<" "<< k<<"   ";
-					
-				}
-			}
-			if (gr[i].neighbour[j] == k)
-			{
-				cout << gr[i].waight[j] << " |";
-				mas[i][k - 1] = gr[i].waight[j];
-
-				j++;
-			}
+			mas[i][j] = INT_MAX - 1;
+		}
+	}
+	for (int i=0;i<tops;i++)
+	{
+		for (int j = 0;j < gr[i].neighbour.size();j++)
+		{
+			mas[i][gr[i].neighbour[j]-1] = gr[i].waight[j];
 
 		}
-		cout << endl;
+	
 	}
+		/*}
+		cout << endl;
+	}*/
 
 }
 minimum findmin(int start,int tops, int *mas,int pos)
@@ -176,14 +223,17 @@ minimum findmin(int start,int tops, int *mas,int pos)
 	}
 	return min;
 }
-void readgraph(node *graph,string buf2,string num,string text,int tops,int edges)
+int readgraph(node *graph,string buf2,string num,string text,int tops,int edges)
 {
+	int res = 0;
 	ifstream imfile;
 	imfile.open("input1.txt");
 	cout << tops << endl;
 	getline(imfile, buf2);
-	getline(imfile, buf2);
-	edges = stoi(buf2);
+	string ty;
+	getline(imfile, ty);
+	cout << ty<<"((((((((((";
+	edges = stoi(ty);
 	cout << edges << endl;
 	for (int i = 0;i < tops;i++)
 	{
@@ -200,16 +250,16 @@ void readgraph(node *graph,string buf2,string num,string text,int tops,int edges
 			}
 			else
 			{
-				graph[i].neighbour[k] = stoi(num);
+				graph[i].neighbour.push_back(stoi(num));
 				num =' ';
 				k++;
 				
 			}
 		
 		}
-		graph[i].neighbour[k] = stoi(num);
+		graph[i].neighbour.push_back(stoi(num));
 		num = ' ';
-		for (int o = 0;o < 10;o++)
+		for (int o = 0;o < graph[i].neighbour.size();o++)
 			if (graph[i].neighbour[o]>0)
 			cout << graph[i].neighbour[o] << ' ';
 		cout << endl;
@@ -223,19 +273,20 @@ void readgraph(node *graph,string buf2,string num,string text,int tops,int edges
 			}
 			else
 			{
-				graph[i].waight[k] = stoi(num);
+				graph[i].waight.push_back(stoi(num));
 				num = ' ';
 				k++;
 			}
 		}
-		graph[i].waight[k] = stoi(num);
+		graph[i].waight.push_back(stoi(num));
 		num = ' ';
-		for (int o = 0;o < 10;o++)
+		for (int o = 0;o < graph[i].waight.size();o++)
 			if (graph[i].waight[o]>0)
 			cout << graph[i].waight[o] << ' ';
 		cout << endl;
 	}
-
+	res=edges;
+	return res;
 }
 int insertsortpos(int *mas, int len, int key)
 {
@@ -271,11 +322,11 @@ int main()
 	int edges = 0;
 	int menu = INT_MIN;
 	string text;
-	string buf1,buf2;
+	string buf1, buf2;
 	string num;
 	int pos = 0;
 	minimum min;
-	
+
 	cout << "Read from file? (1-yep/0-no) ";
 	cin >> menu;
 	cout << endl;
@@ -301,15 +352,18 @@ int main()
 	node *graph = new node[tops];
 	int **mas = new int*[tops];
 	node *resPrim = new node[tops];			//результат для Прима
+	node *resKrus = new node[tops];			//результат для Прима
 	minimum *masmin = new minimum[tops];
 	for (int i = 0;i < tops;i++)
 	{
 		mas[i] = new int[tops];
 		masmin[i].min = 0;
 	}
+
 	if (menu == 1)
 	{
-	readgraph(graph, buf2, num, text, tops, edges);
+		edges=readgraph(graph, buf2, num, text, tops, edges);
+		cout << edges << "<------";
 	}
 	else
 	{
@@ -317,17 +371,17 @@ int main()
 		cin >> edges;
 		cout << endl;
 		node *graph = new node[tops];
-		putgraph(graph, tops, edges);
+	//	putgraph(graph, tops, edges);
 	}
 	for (int i = 0;i < tops;i++)
-		graph[i].IsBlue = false;
-	make_matrix_neigh(graph, tops, edges);
+		graph[i].color = 0;
+//	make_matrix_neigh(graph, tops, edges);
 	cout << endl;
 	cout << endl;
-	make_matrix_wei(graph, tops, edges,mas);
+	make_matrix_wei(graph, tops, mas);
 	cout << endl;
-	getgraph(graph, tops, edges);
-	bool minfound=false;
+	getgraph(graph, tops);
+	bool minfound = false;
 	cout << endl;
 	outp(mas, tops);
 	bool *str = new bool[tops];				//СТРОКИ, КОТОРЫЕ ВКЛЮЧЕНЫ В ПОИСК МИНИМУМА
@@ -337,35 +391,38 @@ int main()
 	for (int i = 0;i < tops;i++)
 		strcycle[i] = false;
 	int isfullstr = 0;
-	
-	
+
+	node *masKr = new node[tops];
+	for (int i = 0;i < tops;i++)
+	{
+		masKr[i] = graph[i];
+	}
+
 	str[0] = true;			//первую строку открыли для поиска
 	strcycle[0] = true;
-	graph[0].IsBlue = true;
+	graph[0].color = 1;
 	int k = 0;
-	int *massivekoef = new int[tops];
-	for (int i = 0;i < tops;i++)
-		massivekoef[i] = -1;
+
 
 
 	while (k < tops - 1)			//идём по вершинам
 	{
 		minimum minimus;
 		minimus.min = INT_MAX;
-		for (int i = 0;i < tops ;i++)
+		for (int i = 0;i < tops;i++)
 		{
-		
+
 			if (str[i] == true)			//идём по открытым вершинам
 			{
 				int popo = 0;
 				int kr;
-				while ((graph[findmin(popo, tops, mas[i], pos).pos].IsBlue == true) && (graph[i].IsBlue == true))
+				while ((graph[findmin(popo, tops, mas[i], pos).pos].color == 1) && (graph[i].color == 1))
 				{
 					//cout << findmin(popo, tops, mas[i], pos).min<< "  BAD REBRO " << i + 1 << "," << findmin(popo, tops, mas[i], pos).pos + 1 << "  " << graph[findmin(popo, tops, mas[i], pos).pos].IsBlue << " " << graph[i].IsBlue << endl;
 					kr = findmin(popo, tops, mas[i], pos).min;
 					//cout << "kr " << kr << endl;
 					popo = kr;
-					if (popo == INT_MAX-1) break;
+					if (popo == INT_MAX - 1) break;
 				}
 				if (findmin(popo, tops, mas[i], pos).min < INT_MAX)
 					minfound = true;		//если минимум остался
@@ -373,31 +430,31 @@ int main()
 				{
 					//cout << "  str[" << i << "] == true  ";
 					//cout << "truemin  ";
-				
+
 					masmin[i] = findmin(popo, tops, mas[i], pos);
 					//cout << "masmin[" << i << "].min" << masmin[i].min << "  ";
-					
+
 					cout << masmin[i].pos << " <------ " << masmin[i].min << "   ";
 					//cout << "REBRO " <<i+1<<","<< masmin[i].pos+1 <<"  "<< graph[masmin[i].pos].IsBlue<<" " << graph[i].IsBlue << endl;
-					if ((masmin[i].min < minimus.min)&&(((graph[masmin[i].pos].IsBlue == false)&& (graph[i].IsBlue == true))|| ((graph[masmin[i].pos].IsBlue == true) && (graph[i].IsBlue == false))))//запоминаем минимальный по открытым строкам
+					if ((masmin[i].min < minimus.min) && (((graph[masmin[i].pos].color == 0) && (graph[i].color == 1)) || ((graph[masmin[i].pos].color == 1) && (graph[i].color == 0))))//запоминаем минимальный по открытым строкам
 					{
 						//cout << " ___entered___" << graph[masmin[i].pos].IsBlue << endl;
 						minimus.min = masmin[i].min;
 						minimus.pos = masmin[i].pos;
-						minimus.str = i;	
+						minimus.str = i;
 					}
-					
+
 					minfound = false;
 
-						
-						cout << endl;
+
+					cout << endl;
 				}
 			}
 		}
-		if (graph[minimus.pos].IsBlue == false)
+		if (graph[minimus.pos].color == 0)
 		{
-			graph[minimus.pos].IsBlue = true;
-			cout << "  top " << minimus.pos+1 << " added to blue tops "<<endl;
+			graph[minimus.pos].color = 1;
+			cout << "  top " << minimus.pos + 1 << " added to blue tops " << endl;
 		}
 		if (strcycle[minimus.pos] == false)
 		{
@@ -405,13 +462,13 @@ int main()
 			isfullstr++;
 		}
 		if (isfullstr + 1 == tops)
-			for (int u = 0;u < tops ;u++)
+			for (int u = 0;u < tops;u++)
 				if (strcycle[u] == false) strcycle[u] == true;
-		cout << "MINIMUS---->" << minimus.min <<" POS---->"<< minimus.pos<< " STR---->" << minimus.str << endl;
-		massivekoef[minimus.str]++;
-		resPrim[minimus.str].neighbour[massivekoef[minimus.str]]=minimus.pos+1;
-		resPrim[minimus.str].waight[massivekoef[minimus.str]] = minimus.min;
-		resPrim[minimus.str].top = minimus.str+1;
+		cout << "MINIMUS---->" << minimus.min << " POS---->" << minimus.pos << " STR---->" << minimus.str << endl;
+
+		resPrim[minimus.str].neighbour.push_back(minimus.pos + 1);
+		resPrim[minimus.str].waight.push_back(minimus.min);
+		resPrim[minimus.str].top = minimus.str + 1;
 
 		k++;
 		mas[minimus.str][minimus.pos] = INT_MAX;
@@ -420,25 +477,160 @@ int main()
 		for (int i = 0;i < tops;i++)
 			str[i] = strcycle[i];
 	}
-	for (int i = 0;i < tops-1;i++)		//вывод
+	for (int i = 0;i < tops;i++)		//вывод
 	{
 		int f = 0;
-		if (resPrim[i].top>0)
-		cout << resPrim[i].top << endl;
-		while (resPrim[i].neighbour[f] > 0)
+		if (resPrim[i].top > 0)
+			cout << resPrim[i].top << endl;
+		while (f<resPrim[i].neighbour.size())
 		{
 			cout << resPrim[i].neighbour[f] << "  ";
-			
+
 			f++;
 		}
 		cout << endl;
 		int fu = 0;
-		while (resPrim[i].waight[fu] > 0)
+		while (fu <resPrim[i].waight.size())
 		{
-			cout << resPrim[i].waight[fu]<<"  " ;
+			cout << resPrim[i].waight[fu] << "  ";
 			fu++;
 		}
 		cout << endl;
+
 		cout << endl;
-	}   
+	}
+
+	make_matrix_wei(resPrim, tops, mas);
+	outp(mas, tops);
+
+
+	int RIB = edges*2;
+	rib *Kr = new rib[RIB];
+	int u = 0;
+	while (u < RIB)
+	{
+		cout << edges << "<----";
+		for (int j = 0;j < tops;j++)
+		{
+			for (int k = 0;k < masKr[j].neighbour.size();k++)
+			{
+				Kr[u].beg = j+1;
+				cout << "Kr[u].beg = " << Kr[u].beg;
+				Kr[u].end = masKr[j].neighbour[k];
+				cout << " Kr[u].end = " << Kr[u].end;
+				Kr[u].wai = masKr[j].waight[k];
+				cout << " Kr[u].wai = " << Kr[u].wai;
+				cout << endl;
+				u++;
+			}
+		}
+	}
+	cout << endl;
+	sortnode(Kr, RIB);
+	for(int u=0;u<RIB;u++)
+	{
+		
+		cout << "Kr[u].beg = " << Kr[u].beg;
+		
+		cout << " Kr[u].end = " << Kr[u].end;
+		
+		cout << " Kr[u].wai = " << Kr[u].wai;
+		cout << endl;
+	}
+	cout << endl;
+	int i = 0;
+	int countmn = 2;
+	masKr[Kr[0].beg-1].color = 1;
+	for (int j = 0;j < RIB;j++)
+	{
+		cout << "color"<< masKr[Kr[j].beg-1].top <<"---" << masKr[Kr[j].beg - 1].color << "---color"<< masKr[Kr[j].end-1].top<<"---" << masKr[Kr[j].end - 1].color << endl;
+		if (((masKr[Kr[j].beg-1].color ==0) && (masKr[Kr[j].end-1].color !=0))|| ((masKr[Kr[j].beg - 1].color != 0) && (masKr[Kr[j].end - 1].color == 0)))//||((masKr[Kr[j].beg - 1].color != masKr[Kr[j].end - 1].color)))
+		{
+			
+			cout << "Kr[u].beg = " << Kr[j].beg;
+
+			cout << " Kr[u].end = " << Kr[j].end;
+
+			cout << " Kr[u].wai = " << Kr[j].wai;
+			cout << endl;
+			if (masKr[Kr[j].beg - 1].color == 0)
+			{
+				masKr[Kr[j].beg - 1].color = masKr[Kr[j].end - 1].color;
+				cout << "top " << Kr[j].beg << " get new color "<< masKr[Kr[j].beg - 1].color;
+				cout << endl;
+			}
+			if (masKr[Kr[j].end - 1].color == 0)
+			{
+				masKr[Kr[j].end - 1].color = masKr[Kr[j].beg - 1].color;
+				cout << "top " << Kr[j].end << " get new color " << masKr[Kr[j].end - 1].color;
+				cout << endl;
+			}
+		}
+		if ((masKr[Kr[j].beg - 1].color == 0) && (masKr[Kr[j].end - 1].color == 0))
+		{
+			cout << "Kr[u].beg = " << Kr[j].beg;
+
+			cout << " Kr[u].end = " << Kr[j].end;
+
+			cout << " Kr[u].wai = " << Kr[j].wai;
+			cout << endl;
+			masKr[Kr[j].beg - 1].color = countmn;
+			cout << "top " << Kr[j].beg << " was opened "<< masKr[Kr[j].beg - 1].color;
+			cout << endl;
+			masKr[Kr[j].end - 1].color = countmn;
+			cout << "top " << Kr[j].end << " was opened "<< masKr[Kr[j].end - 1].color;
+			cout << endl;
+			countmn++;
+		}
+		if (((masKr[Kr[j].beg - 1].color != masKr[Kr[j].end - 1].color)) && ((masKr[Kr[j].beg - 1].color > 0) && (masKr[Kr[j].end - 1].color > 0)))
+		{
+
+			cout << "Kr[u].beg = " << Kr[j].beg;
+
+			cout << " Kr[u].end = " << Kr[j].end;
+
+			cout << " Kr[u].wai = " << Kr[j].wai;
+			cout << endl;
+			if (masKr[Kr[j].end - 1].color > masKr[Kr[j].beg - 1].color)
+			{
+				cout << "hey, " << masKr[Kr[j].end - 1].color << " > " << masKr[Kr[j].beg - 1].color << endl;
+				masKr[Kr[j].end - 1].color= masKr[Kr[j].beg - 1].color;
+				cout << "top " << Kr[j].end << " was changed, color: "<< masKr[Kr[j].end - 1].color;
+				cout << endl;
+				for (int y = 0;y < masKr[Kr[j].end - 1].neighbour.size();y++)
+				{
+					if (masKr[masKr[Kr[j].end - 1].neighbour[y] - 1].color > 1)
+					{
+						cout << "--color--" << masKr[masKr[Kr[j].end - 1].neighbour[y] - 1].color;
+						masKr[masKr[Kr[j].end - 1].neighbour[y] - 1].color= masKr[Kr[j].beg - 1].color;
+						cout << " from " << masKr[masKr[Kr[j].end - 1].neighbour[y] - 1].top << " <--- was changed " << endl;
+					}
+				}
+			}
+			else //if (masKr[Kr[j].end - 1].color < masKr[Kr[j].beg - 1].color)
+			{
+				cout << "hey, " << masKr[Kr[j].beg - 1].color << " > " << masKr[Kr[j].end - 1].color << endl;
+				masKr[Kr[j].beg - 1].color= masKr[Kr[j].end - 1].color;
+				cout << "top " << Kr[j].beg << " was changed, color: " << masKr[Kr[j].beg - 1].color;
+				cout << endl;
+				for (int y = 0;y < masKr[Kr[j].beg - 1].neighbour.size();y++)
+				{
+					if (masKr[masKr[Kr[j].beg - 1].neighbour[y] - 1].color)
+					{
+						cout << "--color--" << masKr[masKr[Kr[j].beg - 1].neighbour[y] - 1].color;
+						masKr[masKr[Kr[j].beg - 1].neighbour[y] - 1].color= masKr[Kr[j].end - 1].color;
+						cout << " from " << masKr[masKr[Kr[j].beg - 1].neighbour[y] - 1].top << " <--- was changed " << endl;;
+					}
+				}
+			}
+		}
+
+
+		
+
+
+
+		i++;
+	}
+
 }
--- Aias/Source1.cpp
+++ Aias/Source1.cpp
@@ -7,47 +7,27 @@
 //#include <fstream>
 //#include <string>
 //#include <algorithm>
+//# include <conio.h>
 //#include <vector>
-//#include <queue>
-//#include <functional>   // std::greater
-//#include <cassert>
+//
 //using namespace std;
 //
-//const int INF = 1e9 + 7;
+//struct node
+//{
+//	vector<int> neighbour;
+//	vector<int> waight;
+//	//int *neighbour = new int[100];
+//	//int *waight = new int[100];
+//	int top;
+//	bool IsBlue;
+//};
+//int main()
+//{
+//	node tum;
+//	tum.top = 1;
+//	tum.neighbour.push_back(3);
+//	cout << tum.top << " " << tum.neighbour[0];
 //
-//vector<pair<int, int>> graph[100000];
-//bool used[100000];      //включили ли мы соответствующую вершину в остов
 //
-//int main() {
-//	//Ввод графа...
 //
-//	int mst_weight = 0;     //Текущий вес остова.
-//
-//	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-//
-//	q.push({ 0, 0 });     //Начнём с вершины 0.
-//
-//	while (!q.empty()) {
-//		pair<int, int> c = q.top();
-//		q.pop();
-//
-//		int dst = c.first, v = c.second;
-//
-//		if (used[v]) {      //вершина уже добавлена в остов
-//			continue;
-//		}
-//
-//		used[v] = true;
-//		mst_weight += dst;
-//
-//		for (pair<int, int> e : graph[v]) {
-//			int u = e.first, len_vu = e.second;
-//
-//			if (!used[u]) {
-//				q.push({ len_vu, u });    //Заметьте: мы учитываем только длину ребра.
-//			}
-//		}
-//	}
-//
-//	cout << "Minimum spanning tree weight: " << mst_weight << endl;
 //}
